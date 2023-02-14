4bc3be1b3fdd42b010a73c8c98eb1cd1595c2b5a
wangshanxu <2661309223@qq.com>
2023-02-14 16:03:24 +0800

    =====提交=====

--- src/test/java/com/wuwei/redisdemo1/Redisdemo1ApplicationTests.java
+++ src/test/java/com/wuwei/redisdemo1/Redisdemo1ApplicationTests.java
@@ -1,20 +1,17 @@
 package com.wuwei.redisdemo1;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.wuwei.redisdemo1.mapper.UserMapper;
-import com.wuwei.redisdemo1.po.User;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Redisdemo1ApplicationTests {
-    @Autowired
-    UserMapper userMapper;
+
     @Test
     void contextLoads() {
         System.out.println("git add");
         System.out.println("commit add");
+        System.out.println("分支");
+        System.out.println("a");
     }
 
 }
